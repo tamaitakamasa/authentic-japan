214d19566b2df41c609f6b53a6591ed3c3c0a537
tamai <tamai@shimatoworks.jp>
2024-10-08 12:55:34 +0900

    ページ追加テスト

---
+++ app/[lang]/news/page.tsx
@@ -0,0 +1,23 @@
+// import Image from 'next/image';
+// import { ContentHeader } from '@/components';
+// import TestLink from '@/components/TestLink';
+import { Locale } from '@/constants/site';
+import Link from 'next/link';
+
+export default async function Home({ params: { lang } }: { params: { lang: Locale } }) {
+
+	return (
+		<>
+			{/* <ContentHeader
+        title="NEWS"
+        breadcrumbs={[
+          { label: 'HOME', href: '/' },
+          { label: 'NEWS' }
+        ]}
+        lang={lang}
+      /> */}
+			<Link href={`/${lang}`}>HOME</Link>
+			{/* <TestLink /> */}
+		</>
+	);
+}
