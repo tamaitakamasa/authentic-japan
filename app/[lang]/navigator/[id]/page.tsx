f3e34f20d3773cedf7dfae65a3fe75d7b2fd39af
tamai <tamai@shimatoworks.jp>
2024-10-10 18:55:47 +0900

    関連するニュース追加

--- app/[lang]/navigator/[id]/page.tsx
+++ app/[lang]/navigator/[id]/page.tsx
@@ -2,18 +2,13 @@ import Image from 'next/image';
 import { Locale } from '@/constants/site';
 // import { useTranslations } from '@/lib/i18n';
 import { notFound } from 'next/navigation';
-import { getFormattedActivities, getFormattedGuideData } from '@/lib/utils';
+import { getFormattedActivities, getFormattedGuideData, getFormattedNewsData } from '@/lib/utils';
 import { ContentHeader } from '@/components/Layout/ContentHeader';
 import { TourItem } from '@/components/Tour/TourItem';
 import Link from 'next/link';
-import { fetchNewsArticles } from '@/lib/fetchData';
-
-// export async function generateStaticParams({ params: { lang } }: { params: { lang: Locale } }) {
-// 	const guides = await extractGuideData(lang);
-// 	return guides.map((guide) => ({
-// 		id: guide.id.toString()
-// 	}));
-// }
+// import { fetchNewsArticles } from '@/lib/fetchData';
+import NavigatorInfo from '@/components/Navigator/NavigatorInfo';
+import NewsItem from '@/components/News/NewsItem';
 
 export default async function Page({ params: { lang, id } }: { params: { lang: Locale; id: string } }) {
 	// const t = useTranslations(lang);
@@ -21,8 +16,10 @@ export default async function Page({ params: { lang, id } }: { params: { lang: L
 	const otherGuides = guides.filter((g) => g.id !== parseInt(id));
 	const guide = guides.find((g) => g.id === parseInt(id));
 	const activities = await getFormattedActivities({ page: 1, pageSize: 10 }, lang);
-	const newsArticles = await fetchNewsArticles(lang);
+	const newsArticles = await getFormattedNewsData(lang);
 	const filteredActivities = guide ? activities.filter((activity) => activity.guideIds?.includes(guide.id)) : [];
+	const relatedNews = guide ? newsArticles.filter((news) => news.guides?.some((g) => g.id === guide.id)) : [];
+
 	console.log(newsArticles);
 
 	if (!guide) {
@@ -37,57 +34,82 @@ export default async function Page({ params: { lang, id } }: { params: { lang: L
 				<div className="p-page-navigator__profile">
 					<figure className="p-page-navigator__mv">{guide.mv && <Image src={guide.mv.link} alt={guide.name} fill sizes="50vw" style={{ objectFit: 'cover' }} />}</figure>
 					<div className="p-page-navigator__detail">
+						<h2 className="p-page-navigator__copy" dangerouslySetInnerHTML={{ __html: guide.copy || '' }} />
 						{guide.regions && guide.regions.length > 0 && (
 							<div className="p-page-navigator__region">
 								<i className="c-pin"></i>
 								<span>{guide.regions.join(', ')}</span>
 							</div>
 						)}
-						<div className="p-page-navigator__name">{guide.name}</div>
-						<div className="p-page-navigator__title">{guide.title}</div>
+						<NavigatorInfo className="p-page-navigator__info" guide={guide} lang={lang} />
 						<div className="p-page-navigator__tags">
 							{guide.tags.map((tag) => (
-								<span key={tag.id} className="p-page-navigator__tag">
+								<span key={tag.id} className="p-page-navigator__tag c-taxonomy">
 									#{tag.name}
 								</span>
 							))}
 						</div>
-						<h2 className="p-page-navigator__copy" dangerouslySetInnerHTML={{ __html: guide.copy || '' }} />
-						<div className="p-page-navigator__description" dangerouslySetInnerHTML={{ __html: guide.description || '' }} />
-						{/* {guide.values && guide.values.length > 0 && (
-							<>
-								<h2 className="p-page-navigator__values-title">{t({ ja: '私の提供する旅で大切にしたいこと', en: 'What I value in the journey I offer' })}</h2>
-								<div className="p-page-navigator__values">
-									{guide.values.map((value, index) => (
-										<div key={index} className="p-page-navigator__value">
-											<h3>{value.title}</h3>
-											<p dangerouslySetInnerHTML={{ __html: value.description || '' }} />
-										</div>
-									))}
-								</div>
-							</>
-						)} */}
+						<ul className="p-page-navigator__sns">
+							<li>
+								<a href="#">
+									<Image src={'/icon_instagram.svg'} width={15} height={15} alt="" />
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<Image src={'/icon_fb.svg'} width={15} height={15} alt="" />
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<Image src={'/icon_x.svg'} width={15} height={15} alt="" />
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<Image src={'/icon_note.svg'} width={40} height={8.7} alt="" />
+								</a>
+							</li>
+						</ul>
 					</div>
+					<div className="p-page-navigator__description" dangerouslySetInnerHTML={{ __html: guide.description || '' }} />
 				</div>
 
 				{filteredActivities.length > 0 && (
-				<div className="p-page-navigator-tours">
-					<div className="p-page-navigator-tours__title">
-						<h2 className="c-heading c-heading--2">TOURS</h2>
-					</div>
-					<div className="p-page-navigator-tours__tours c-tours">
-						{filteredActivities.map((activity) => <TourItem key={activity.id} activity={activity} className="c-tours__tour" />)}
+					<div className="p-page-navigator-tours">
+						<div className="p-page-navigator-tours__title">
+							<h2 className="c-heading c-heading--2">TOURS</h2>
 						</div>
-				</div>
+						<div className="p-page-navigator-tours__tours c-tours">
+							{filteredActivities.map((activity) => (
+								<TourItem key={activity.id} activity={activity} className="c-tours__tour" />
+							))}
+						</div>
+					</div>
 				)}
 
-				<div className="p-page-navigator-others">
-					<div className="p-page-navigator-others__title">
-						<h2 className="c-heading c-heading--2">OTHER NAVIGATORS</h2>
+				{relatedNews.length > 0 && (
+					<div className="p-page-navigator-news">
+						<div className="p-page-navigator-news__title">
+							<h2 className="c-heading c-heading--2">RELATED NEWS</h2>
+						</div>
+						<div className="p-page-navigator-news__items">
+							{relatedNews.map((article) => (
+								<div key={article.id} className="p-page-news__item">
+									<NewsItem article={article} />
+								</div>
+							))}
+						</div>
 					</div>
-					<div className="p-page-navigator-others__items">
-						{otherGuides.length > 0 ? (
-							otherGuides.map((guide) => (
+				)}
+
+				{otherGuides.length > 0 && (
+					<div className="p-page-navigator-others">
+						<div className="p-page-navigator-others__title">
+							<h2 className="c-heading c-heading--2">OTHER NAVIGATORS</h2>
+						</div>
+						<div className="p-page-navigator-others__items">
+							{otherGuides.map((guide) => (
 								<div key={guide.id} className="p-page-navigator-others__item">
 									<div className="c-navigator-circle u-hover">
 										<figure className="c-navigator-circle__thumb">
@@ -107,12 +129,10 @@ export default async function Page({ params: { lang, id } }: { params: { lang: L
 										)}
 									</div>
 								</div>
-							))
-						) : (
-							<p>ガイドが見つかりません。</p>
-						)}
+							))}
+						</div>
 					</div>
-				</div>
+				)}
 			</div>
 		</>
 	);
