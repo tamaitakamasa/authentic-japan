7e5cace28ae8ee0cc65c33d622cba3aefa0e4e71
tamai <tamai@shimatoworks.jp>
2025-03-06 14:33:48 +0900

    feat: Aboutページのスタイルと構造を改善
    - _c-video.scss: メディアクエリを追加し、特定の条件下での高さを0に設定
    - page.tsx: コードの整形を行い、可読性を向上
    - _p-page-about.scss: スタイルの整理とメディアクエリを追加し、レスポンシブデザインを強化

--- app/[lang]/about/page.tsx
+++ app/[lang]/about/page.tsx
@@ -1,17 +1,19 @@
 // import Image from 'next/image';
-import { ContentHeader } from '@/components/Layout/ContentHeader';
-import { PageHeader } from '@/components/Layout/PageHeader';
-import { VideoPlayer } from '@/components/VideoPlayer';
-import { Locale } from '@/constants/site';
-import { getWPSiteOptions } from '@/lib/fetchData';
-import { Metadata } from 'next';
-import AboutSection from '@/components/About/AboutSection';
+import { ContentHeader } from "@/components/Layout/ContentHeader";
+import { PageHeader } from "@/components/Layout/PageHeader";
+import { VideoPlayer } from "@/components/VideoPlayer";
+import { Locale } from "@/constants/site";
+import { getWPSiteOptions } from "@/lib/fetchData";
+import { Metadata } from "next";
+import AboutSection from "@/components/About/AboutSection";
 
 type Props = {
-  params: { lang: Locale }
-}
+  params: { lang: Locale };
+};
 
-export async function generateMetadata({ params: { lang } }: Props): Promise<Metadata> {
+export async function generateMetadata({
+  params: { lang },
+}: Props): Promise<Metadata> {
   return {
     // タイトルだけをオーバーライド
     // layout.tsxで設定したテンプレートが適用される
@@ -29,21 +31,35 @@ export async function generateMetadata({ params: { lang } }: Props): Promise<Met
   };
 }
 
-export default async function AboutPage({ params: { lang } }: { params: { lang: Locale } }) {
-	const siteOptions = await getWPSiteOptions(lang);
+export default async function AboutPage({
+  params: { lang },
+}: {
+  params: { lang: Locale };
+}) {
+  const siteOptions = await getWPSiteOptions(lang);
 
-	return (
-		<>
-			<ContentHeader title="ABOUT US" breadcrumbs={[{ label: 'HOME', href: '/' }, { label: 'ABOUT US' }]} lang={lang} />
-			<div className="l-contents__body p-page p-page-about">
-				<PageHeader description={siteOptions.about_description} mv={siteOptions.about_mv} />
-				{siteOptions.about_video && (
-					<div className="p-page-about__video">
-						<VideoPlayer videoUrl={siteOptions.about_video} />
-					</div>
-				)}
-				<AboutSection siteOptions={siteOptions} />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <ContentHeader
+        title="ABOUT US"
+        breadcrumbs={[
+          { label: "HOME", href: "/" },
+          { label: "ABOUT US" },
+        ]}
+        lang={lang}
+      />
+      <div className="l-contents__body p-page p-page-about">
+        <PageHeader
+          description={siteOptions.about_description}
+          mv={siteOptions.about_mv}
+        />
+        {siteOptions.about_video && (
+          <div className="p-page-about__video">
+            <VideoPlayer videoUrl={siteOptions.about_video} />
+          </div>
+        )}
+        <AboutSection siteOptions={siteOptions} />
+      </div>
+    </>
+  );
 }
--- components/About/AboutSection.tsx
+++ components/About/AboutSection.tsx
@@ -1,101 +1,161 @@
-'use client';
+"use client";
 
-import { WPSiteContent } from '@/types';
-import Image from 'next/image';
-import { motion } from 'motion/react';
-import { useEffect, useState } from 'react';
+import { WPSiteContent } from "@/types";
+import Image from "next/image";
+import { motion } from "motion/react";
+import { useEffect, useState } from "react";
 
 interface AboutSectionItemProps {
-	number: string;
-	title: string;
-	subtitle: string | undefined;
-	description: string | undefined;
-	color?: string;
+  number: string;
+  title: string;
+  subtitle: string | undefined;
+  description: string | undefined;
+  color?: string;
 }
 
 interface AboutSectionProps {
-	siteOptions: WPSiteContent;
+  siteOptions: WPSiteContent;
 }
 
-function AboutSectionItem({ number, title, subtitle, description, color }: AboutSectionItemProps) {
-	return (
-		<div data-index={number} className={`u-${color} p-page-about__section`}>
-			<motion.div initial={{ opacity: 0, x: -100 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 2, ease: [0.16, 1, 0.3, 1] }}>
-				<hgroup>
-					<motion.h2 initial={{ opacity: 0, y: 40 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.8, delay: 0.2 }}>
-						{number}. {title}
-					</motion.h2>
-					<motion.h3 initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.8, delay: 0.2 }}>
-						{subtitle}
-					</motion.h3>
-				</hgroup>
-				<motion.p dangerouslySetInnerHTML={{ __html: description || '' }} initial={{ opacity: 0, x: -100 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 2, ease: [0.16, 1, 0.3, 1] }} />
-			</motion.div>
-		</div>
-	);
+function AboutSectionItem({
+  number,
+  title,
+  subtitle,
+  description,
+  color,
+}: AboutSectionItemProps) {
+  return (
+    <div
+      data-index={number}
+      className={`u-${color} p-page-about__section`}
+    >
+      <motion.div
+        initial={{ opacity: 0, x: -100 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 2, ease: [0.16, 1, 0.3, 1] }}
+      >
+        <hgroup>
+          <motion.h2
+            initial={{ opacity: 0, y: 40 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.2 }}
+          >
+            {number}. {title}
+          </motion.h2>
+          <motion.h3
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.2 }}
+          >
+            {subtitle}
+          </motion.h3>
+        </hgroup>
+        <motion.p
+          dangerouslySetInnerHTML={{ __html: description || "" }}
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 2, ease: [0.16, 1, 0.3, 1] }}
+        />
+      </motion.div>
+    </div>
+  );
 }
 
-export default function AboutSection({ siteOptions }: AboutSectionProps) {
-	const [activeImageIndex, setActiveImageIndex] = useState(0);
-	// console.log('activeImageIndex:', activeImageIndex);
+export default function AboutSection({
+  siteOptions,
+}: AboutSectionProps) {
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
+  // console.log('activeImageIndex:', activeImageIndex);
 
-	// Intersection Observer を使用してアクティブなサービスアイテムを検出
-	useEffect(() => {
-		const observer = new IntersectionObserver(
-			(entries) => {
-				entries.forEach((entry) => {
-					if (entry.isIntersecting) {
-						const index = Number(entry.target.getAttribute('data-index'));
-						setActiveImageIndex(index);
-						console.log('Section becoming visible:', {
-							index,
-							element: entry.target
-						});
-						// setActiveMenuIndex(index);
-					}
-				});
-			},
-			{
-				threshold: 0.5
-			}
-		);
+  // Intersection Observer を使用してアクティブなサービスアイテムを検出
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const index = Number(
+              entry.target.getAttribute("data-index")
+            );
+            setActiveImageIndex(index);
+            console.log("Section becoming visible:", {
+              index,
+              element: entry.target,
+            });
+            // setActiveMenuIndex(index);
+          }
+        });
+      },
+      {
+        threshold: 0.5,
+      }
+    );
 
-		const items = document.querySelectorAll('.p-page-about__section');
-		items.forEach((item) => observer.observe(item));
+    const items = document.querySelectorAll(".p-page-about__section");
+    items.forEach((item) => observer.observe(item));
 
-		return () => {
-			items.forEach((item) => observer.unobserve(item));
-		};
-	}, []);
+    return () => {
+      items.forEach((item) => observer.unobserve(item));
+    };
+  }, []);
 
-	return (
-		<div className="p-page-about__container">
-			<div className="p-page-about__description">
-				<div data-index="00" className="p-page-about__section">
-					<div>
-						<hgroup>
-							<h3>Authentic Japan Navigator Will Share The Passion</h3>
-						</hgroup>
-						<p dangerouslySetInnerHTML={{ __html: siteOptions.about_section0_description || '' }} />
-					</div>
-				</div>
-				<AboutSectionItem number="01" title="Travelers" subtitle={siteOptions.about_section1_title} description={siteOptions.about_section1_description} color="green" />
-				<AboutSectionItem number="02" title="Authentic Japnan Navigator" subtitle={siteOptions.about_section2_title} description={siteOptions.about_section2_description} color="red" />
-				<AboutSectionItem number="03" title="Local Area" subtitle={siteOptions.about_section3_title} description={siteOptions.about_section3_description} color="blue" />
-			</div>
-			<div className="p-page-about__image">
-				{[...Array(4)].map((_, index) => (
-					<figure
-						key={index}
-						style={{
-							opacity: activeImageIndex === index ? 1 : 0,
-							transition: 'opacity 0.5s ease-in-out'
-						}}>
-						<Image src={`/about/about${index === 0 ? '_all' : index}.png`} alt="" fill unoptimized />
-					</figure>
-				))}
-			</div>
-			{/* <div className="p-page-about__image">
+  return (
+    <div className="p-page-about__container">
+      <div className="p-page-about__description">
+        <div data-index="00" className="p-page-about__section">
+          <div>
+            <hgroup>
+              <h3>
+                Authentic Japan Navigator Will Share The Passion
+              </h3>
+            </hgroup>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: siteOptions.about_section0_description || "",
+              }}
+            />
+          </div>
+        </div>
+        <AboutSectionItem
+          number="01"
+          title="Travelers"
+          subtitle={siteOptions.about_section1_title}
+          description={siteOptions.about_section1_description}
+          color="green"
+        />
+        <AboutSectionItem
+          number="02"
+          title="Authentic Japnan Navigator"
+          subtitle={siteOptions.about_section2_title}
+          description={siteOptions.about_section2_description}
+          color="red"
+        />
+        <AboutSectionItem
+          number="03"
+          title="Local Area"
+          subtitle={siteOptions.about_section3_title}
+          description={siteOptions.about_section3_description}
+          color="blue"
+        />
+      </div>
+      <div className="p-page-about__image">
+        {[...Array(4)].map((_, index) => (
+          <figure
+            key={index}
+            style={{
+              opacity: activeImageIndex === index ? 1 : 0,
+              transition: "opacity 0.5s ease-in-out",
+            }}
+          >
+            <Image
+              src={`/about/about${index === 0 ? "_all" : index}.png`}
+              alt=""
+              fill
+              unoptimized
+            />
+          </figure>
+        ))}
+      </div>
+      {/* <div className="p-page-about__image">
 				<figure>
 					<Image src="/about/about_all.svg" alt="" fill unoptimized />
 				</figure>
@@ -109,6 +169,6 @@ export default function AboutSection({ siteOptions }: AboutSectionProps) {
 					<Image src="/about/about3.svg" alt="" fill unoptimized />
 				</figure>
 			</div> */}
-		</div>
-	);
+    </div>
+  );
 }
